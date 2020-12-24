67466ee4ef811fd58689fee468c27d66b95aa9c8
taka <0bn644063264k0f@ezweb.ne.jp>
2020-12-24 11:30:38 +0000

    add sessions

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   get 'toppages/index'
   root "toppages#index"
   
+  
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  
   get "signup", to: "users#new"
-  resources :users, only: [:new, :create, :destroy]
+  resources :users, only: [:show, :new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
