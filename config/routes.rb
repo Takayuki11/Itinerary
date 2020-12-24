44d913ce25d5eeb98daeb604a31617f3f5d1cc83
taka <0bn644063264k0f@ezweb.ne.jp>
2020-12-24 10:49:58 +0000

    add User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  
   get 'toppages/index'
   root "toppages#index"
+  
+  get "signup", to: "users#new"
+  resources :users, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
