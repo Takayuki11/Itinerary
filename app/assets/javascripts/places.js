b12d508a533f4642938c693042b386b7a3bb74b2
taka <0bn644063264k0f@ezweb.ne.jp>
2021-01-02 09:06:56 +0000

    add responsive

--- app/assets/javascripts/places.js
+++ app/assets/javascripts/places.js
@@ -1,18 +1,25 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(function() {
-    var nav = $('.left');
-    //navの位置    
-    var navTop = nav.offset().top;
-    //スクロールするたびに実行
-    $(window).scroll(function () {
-        var winTop = $(this).scrollTop();
-        //スクロール位置がnavの位置より下だったらクラスfixedを追加
-        if (winTop >= navTop) {
-            nav.addClass('fixed')
-        } else if (winTop <= navTop) {
-            nav.removeClass('fixed')
+const getTop = () => {
+    $(function() {
+        if ($(".left-side").length) {
+            var nav = $(".left-side");
+            //navの位置
+            var navTop = nav.offset().top;
+            console.log(navTop);
+            //スクロールするたびに実行
+            $(window).scroll(function () {
+                var winTop = $(this).scrollTop();
+                //スクロール位置がnavの位置より下だったらクラスfixedを追加
+                if (winTop >= navTop) {
+                    nav.addClass('fixed');
+                } else if (winTop <= navTop) {
+                    nav.removeClass('fixed');
+                }
+            });
         }
     });
-});
+};
+
+$(window).on("load", getTop)
