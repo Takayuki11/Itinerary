8e411ed7ea46b2d5157ebd1e44d7c5c13fd7eadd
taka <[taka.ymgc.11@gmail.com]>
2021-01-09 05:06:19 +0000

    add favorite model

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,13 +5,13 @@ class FavoritesController < ApplicationController
     place = Place.find(params[:place_id])
     current_user.like(place)
     flash[:success] = "お気に入りに追加しました。"
-    redirect_to user_path(current_user)
+    redirect_to place_path(place)
   end
 
   def destroy
     place = Place.find(params[:place_id])
     current_user.unlike(place)
     flash[:success] = "お気に入りを解除しました。"
-    redirect_to user_path(current_user)
+    redirect_to place_path(place)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,8 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @places = @user.places
+    counts(@user)
   end
   
   def new
